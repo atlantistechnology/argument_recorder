1c9f75100d6ad6c1d04851d31edb473ea8a16d1e
Jon Collier <jcollier@atlantistech.com>
2020-03-06 13:48:16 -0500

    Initial working version!

--- lib/argument_recorder.rb
+++ lib/argument_recorder.rb
@@ -1,6 +1,68 @@
 require 'argument_recorder/version'
+require 'pry'
 
 module ArgumentRecorder
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.included(parent_class)
+    parent_class.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def record_arguments
+      @argument_recordings = {}
+
+      relevant_methods_names.each do |method_name| # symbol
+        @argument_recordings[method_name] = {}
+
+        # type can be:
+        # :req - required argument
+        # :opt - optional argument
+        # :rest - rest of arguments as array
+        # :keyreq - reguired key argument (2.1+)
+        # :key - key argument
+        # :keyrest - rest of key arguments as Hash
+        # :block - block parameter
+        instance_method(method_name).parameters.each do |type, parameter_name|
+          @argument_recordings[method_name][parameter_name] = {
+            type: type,
+            examples: [],
+          }
+        end
+        # binding.pry
+
+        # Copy the original method
+        alias_method "original_#{method_name}".to_sym, method_name
+
+        # Remove the original method
+        remove_method method_name
+
+        # Redifine the method
+        define_method(method_name) do |*arguments|
+          # Record data about the arguments
+          self.class.instance_variable_get(:@argument_recordings)[method_name].tap do |method_storage|
+            arguments.each_with_index do |argument_value, argument_index|
+              if argument_value.is_a?(Hash)
+                argument_value.each do |key, value|
+                  method_storage[key][:examples].push(value)
+                end
+              else
+                method_storage[method_storage.keys[argument_index]][:examples].push argument_value
+              end
+            end
+          end
+
+          # Call the original method
+          send("original_#{method_name}".to_sym, *arguments)
+        end
+      end
+    end
+
+    def relevant_methods_names
+      (instance_methods - Object.methods).select do |method_name|
+        instance_method(method_name).arity.positive?
+      end
+    end
+  end
 end
--- spec/argument_recorder_spec.rb
+++ spec/argument_recorder_spec.rb
@@ -1,5 +1,34 @@
 RSpec.describe ArgumentRecorder do
+  class SampleClass
+    include ArgumentRecorder
+
+    def add(number1, number2)
+      number1 + number2
+    end
+
+    def display_icon(kind: 'alert', message:)
+      "#{kind} : #{message}"
+    end
+
+    record_arguments
+  end
+
   it 'has a version number' do
     expect(ArgumentRecorder::VERSION).not_to be nil
+  end
+
+  it 'calls method with simple required arguments' do
+    puts "1 plus 5 is #{SampleClass.new.add(1, 5)}"
+    puts "2 plus 9 is #{SampleClass.new.add(2, 9)}"
+    puts "3 plus 27 is #{SampleClass.new.add(3, 27)}"
+  end
+
+  it 'calls method with a keyword containing default' do
+    puts SampleClass.new.display_icon(kind: :notice, message: "It's OK")
+    puts SampleClass.new.display_icon(kind: :alert, message: 'SOMETHING IS WRONG')
+  end
+
+  it 'recorded something' do
+    expect(SampleClass.instance_variable_get(:@argument_recordings)).to eq({ add: {} })
   end
 end
